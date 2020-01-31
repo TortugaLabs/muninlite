cdb7363c480b0a91405b74d2ff5dc2df86d1acf5
Alejandro Liu <alejandro_liu@hotmail.com>
2020-01-31 10:59:44 +0100

    more flexible df plugin

--- plugins/df.sh
+++ plugins/df.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-filter_fs() {
+df_filter_fs() {
   local fstype=$(awk '$2 == "'"$1"'" { print $3 }' /proc/mounts)
   case "$fstype" in
   iso9660) return 1 ;;
@@ -8,16 +8,21 @@ filter_fs() {
   return 0
 }
 
+df_calc_threshold() {
+  local total="$1"
+  local val="$(expr "$2" '*' 1048576)" # Convert input to gigs
+  echo "$total $val" | awk '{ print 100 - ($2/$1*100) }'
+}
 
 config_df() {
-  echo "graph_title Filesystem usage (in %)
+  echo "graph_title xxFilesystem usage (in %)
 graph_args --upper-limit 100 -l 0
 graph_vlabel %
 graph_category disk
 graph_info This graph shows disk usage on the machine."
   for PART in $(df -P | grep '^/' | sed '/\/[a-z0-9,]*$/!d;s/.* \([a-z0-9,\/]\{1,\}\)$/\1/g')
   do
-    filter_fs $PART || continue
+    df_filter_fs $PART || continue
     PINFO=$(df -P $PART | tail -1)
     PNAME=$(echo $PINFO | cut -d\  -f1 | sed 's/\//_/g')
     PSIZE=$( (df -Ph $PART 2>/dev/null || df -P $PART)| tail -1 | awk '{print $2}')
@@ -25,22 +30,23 @@ graph_info This graph shows disk usage on the machine."
     PAVAIL=$( (df -Ph $PART 2>/dev/null || df -P $PART)| tail -1 | awk '{print $4}')
     echo "$PNAME.label $PART ($PSIZE)"
     echo "$PNAME.info $PNAME -> $PART $PAVAIL free ($PTYPE)"
-    ONEPCT=$(echo $PINFO | awk '{ print int($2 / 100)}')
-    if [ $ONEPCT -lt 10000000 ] ; then # 1% is less than 10G!
-      echo "$PNAME.warning 92"
-      echo "$PNAME.critical 98"
+    ks=$(echo $PINFO | awk '{print $2}')
+    if [ $ks -lt 104857600 ] ; then # FileSystem is less than 100G
+      echo "$PNAME.warning 90.0"
+      echo "$PNAME.critical 95.0"
     else
-      echo "$PNAME.warning 98"
-      echo "$PNAME.critical 99"
-    fi
+      # For more than 100G, the tresholds are based actual storage (not fixed percentage)
+      echo "$PNAME.warning $(df_calc_threshold $ks 20)"
+      echo "$PNAME.critical $(df_calc_threshold $ks 2)"
+    fi      
   done
 }
 fetch_df() {
   for PART in $(df -P | grep '^/' | sed '/\/[a-z0-9,]*$/!d;s/.* \([a-z0-9,\/]\{1,\}\)$/\1/g')
   do
-    filter_fs $PART || continue
+    df_filter_fs $PART || continue
     PINFO=$(df -P $PART | tail -1);
     PNAME=$(echo $PINFO | cut -d\  -f1 | sed 's/[\/.-]/_/g')
-    echo "$PNAME.value" $(echo $PINFO | cut -f5 -d\  | sed -e 's/\%//g')
+    echo "$PNAME.value" $(echo $PINFO | awk '{ print $3/$2*100 }')
   done
 }
